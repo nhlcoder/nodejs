17195ff902339e54d54e7fa1776f1971683a80a2
Nguyễn Hải Lý <nguyenhaily@Nguyen-Hai-Ly.local>
2023-05-06 17:31:30 +0700

    Second Update

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// HTTP Logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine("hbs", handlebars.engine({
+    extname: '.hbs'
+}));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get('/',(req, res) => {
+    return res.render('home');
+});
+app.get('/new',(req, res) => {
+    return res.render('new');
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listen on port http://localhost:${port}`);
+});
